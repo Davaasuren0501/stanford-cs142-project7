9ae740e87e3810198f22a0701abdb7a75449dfc1
Davaasuren0501 <davaasuren@alldomain.biz>
2022-12-20 12:49:20 +0800

    login user id pass url

--- components/loginRegister/login.jsx
+++ components/loginRegister/login.jsx
@@ -1,54 +1,3 @@
-// import React, { useState } from "react";
-// import { HashRouter, Link, Router } from "react-router-dom";
-// import axios from "axios";
-
-// export default function Login() {
-//   const [loginInfo, setLoginInfo] = useState({
-//     name: null,
-//     pass: null,
-//   });
-
-//   const _login = () => {
-//     console.log(loginInfo);
-//   };
-
-//   return (
-//     <div>
-//       <h3>
-//         Please log in with your username and password.
-//         <br />
-//       </h3>
-//       If you do not have an account, please register as a new user.
-//       <br />
-//       <br />
-//       <label>Username: </label>
-//       <input
-//         type="text"
-//         onChange={(e) => {
-//           setLoginInfo({ ...loginInfo, name: e.target.value });
-//         }}
-//       />
-//       <br />
-//       <br />
-//       <label>Password: </label>
-//       <input
-//         type="password"
-//         onChange={(e) => {
-//           setLoginInfo({ ...loginInfo, pass: e.target.value });
-//         }}
-//       />
-//       <br />
-//       <br />
-//       <button type="button" onClick={_login}>
-//         Login
-//       </button>
-//       <span> </span>
-//       <Link to={`/admin/register`}>
-//         <button type="button">Register</button>
-//       </Link>
-//     </div>
-//   );
-// }
 import React from "react";
 import axios from "axios";
 
@@ -101,8 +50,9 @@ class Login extends React.Component {
               isLoginNameOrPasswordWrong: false,
             });
             sessionStorage.setItem("isLoggedIn", true);
-            window.location.reload();
+            //
             window.location.assign(userDetailLink);
+            window.location.reload();
           })
           .catch((err) => {
             console.log(err);
